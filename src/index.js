93b89e285e321ddc6f10b852a2c408313774a2f9
flavio manzotti <manzottiflavio22@gmail.com>
2021-12-14 15:40:16 -0300

    rotas criadas com sucesso

--- src/index.js
+++ src/index.js
@@ -7,5 +7,21 @@ app.use=(express.json());
 
 
 app.get("/aula", (request, response)=>{
-    return response.json({menssagem:"ola"})
+    return response.json({menssagem:"ola"})/*sempre lembrar de colocar em chave{} ou couchetes[]*/
+})
+
+app.post("/aula", (request,response)=>{
+    return response.json(["aula1", "aula2"])
+});
+
+app.put("/aula/:id",(request, response)=>{
+    return response.json(["aula1", "aula2","aula3"])
+});
+
+app.patch("/aula/:id", (request, response)=>{
+    return response.json(["aula1","aula2", "aula4"])
+});
+
+app.delete("/aula/:id", (request, response)=>{
+    return response.json(["aula1","aula2"])
 })
